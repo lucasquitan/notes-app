1ee3636617090d765f599ac063bf9b357f5a2647
Lucas Quintanilha <l.quitan@gmail.com>
2020-02-27 11:03:40 -0300

    Code Lint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base', 'prettier'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-readdign": "off",
+    "camelcase": "off",
+    "no-console": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
